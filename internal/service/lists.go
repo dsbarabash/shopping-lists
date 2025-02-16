685fe6e4150d7342f7ee0662518dd509596c33b9
Daria <dary.barabash@yandex.ru>
2025-02-16 14:02:08 +0300

    HW-6 update returned value

--- internal/service/lists.go
+++ internal/service/lists.go
@@ -2,12 +2,12 @@ package service
 
 import "github.com/dsbarabash/shopping-lists/internal/model"
 
-func CreateShoppingList(name string, userId string) model.ShoppingLists {
+func CreateShoppingList(name string, userId string) model.ShoppingList {
 	ShoppingList := model.NewShoppingList(name, userId)
 	return ShoppingList
 }
 
-func CreateItem(name string, comment string, userId string, shoppingListId string) model.Items {
+func CreateItem(name string, comment string, userId string, shoppingListId string) model.Item {
 	Item := model.NewItem(name, comment, shoppingListId, userId)
 	return Item
 }
