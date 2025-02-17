d36c0ddb1b64a534cd194e6673e80d3c83034bb6
Daria <dary.barabash@yandex.ru>
2025-02-17 14:47:42 +0300

    HW-6 update type of slices at repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -5,15 +5,15 @@ import (
 	"github.com/dsbarabash/shopping-lists/internal/model"
 )
 
-var ShoppingList = make([]interface{}, 0)
-var Item = make([]interface{}, 0)
+var ShoppingList = make([]model.ShoppingList, 0)
+var Item = make([]model.Item, 0)
 
 func CheckInterface(arg interface{}) {
 	switch arg.(type) {
 	case model.ShoppingLists:
-		ShoppingList = append(ShoppingList, arg)
+		ShoppingList = append(ShoppingList, arg.(model.ShoppingList))
 	case model.Items:
-		Item = append(Item, arg)
+		Item = append(Item, arg.(model.Item))
 	default:
 		fmt.Println("Неизвестный тип ")
 	}
