27f16907aba60b9537e55c7785783885f23c9177
Daria <dary.barabash@yandex.ru>
2025-02-19 23:27:21 +0300

    add pointers to interfaces

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,37 +1,20 @@
 package model
 
+import (
+	"github.com/google/uuid"
+)
+
 type User struct {
 	id    string
 	name  string
 	state State
 }
 
-func (u User) SetId(id string) {
-	u.id = id
-}
-
-func (u User) GetId() string {
-	return u.id
-}
-
-func (u User) SetName(name string) {
-	u.name = name
-}
-
-func (u User) GetName() string {
-	return u.name
-}
-
-func (u User) SetState(state State) {
-	u.state = state
-}
-
-func (u User) GetState() string {
-	return u.state.String()
-}
-
-func NewUser(id string, name string) *User {
-	u := User{id: id, name: name}
-	u.state = 1
-	return &u
+func NewUser(name string) *User {
+	id := uuid.New()
+	return &User{
+		id:    id.String(),
+		name:  name,
+		state: 1,
+	}
 }
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -5,18 +5,18 @@ import (
 	"github.com/dsbarabash/shopping-lists/internal/model"
 )
 
-var ShoppingList = make([]model.ShoppingList, 0)
-var Item = make([]model.Item, 0)
+var ShoppingListSlice = make([]model.ShoppingList, 0)
+var ItemSlice = make([]model.Item, 0)
 
 func CheckInterface(arg interface{}) {
 	switch arg.(type) {
 	case model.ShoppingLists:
-		ShoppingList = append(ShoppingList, arg.(model.ShoppingList))
+		ShoppingListSlice = append(ShoppingListSlice, *arg.(*model.ShoppingList))
 	case model.Items:
-		Item = append(Item, arg.(model.Item))
+		ItemSlice = append(ItemSlice, *arg.(*model.Item))
 	default:
 		fmt.Println("Неизвестный тип ")
 	}
-	fmt.Println("ShoppingList: ", ShoppingList)
-	fmt.Println("Item: ", Item)
+	fmt.Println("ShoppingList: ", ShoppingListSlice)
+	fmt.Println("Item: ", ItemSlice)
 }
