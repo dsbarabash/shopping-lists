80b7e715441f82993e874ffb152355d6a9d6d4e2
Daria <dary.barabash@yandex.ru>
2025-03-02 17:52:10 +0300

    HW-7 Add chan to functions

--- internal/service/lists.go
+++ internal/service/lists.go
@@ -1,6 +1,47 @@
 package service
 
-import "github.com/dsbarabash/shopping-lists/internal/model"
+import (
+	"fmt"
+	"github.com/dsbarabash/shopping-lists/internal/model"
+	"math/rand/v2"
+	"strings"
+)
+
+func CreateRandomStructs(ch chan<- interface{}) {
+	rnd := rand.IntN(100)
+	if rnd%2 == 0 {
+		ShoppingList, err := model.NewShoppingList(randomSLData())
+		if err != nil {
+			panic(err)
+		}
+		ch <- ShoppingList
+		fmt.Println()
+	} else {
+		Item, err := model.NewItem(randomIData())
+		if err != nil {
+			panic(err)
+		}
+		ch <- Item
+	}
+}
+
+func randomSLData() (string, string) {
+	return randomString(10), randomString(10)
+}
+
+func randomIData() (string, string, string, string) {
+	return randomString(10), randomString(10), randomString(10), randomString(10)
+}
+
+func randomString(n int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	sb := strings.Builder{}
+	sb.Grow(n)
+	for i := 0; i < n; i++ {
+		sb.WriteByte(charset[rand.IntN(len(charset))])
+	}
+	return sb.String()
+}
 
 func CreateShoppingList(name string, userId string) *model.ShoppingList {
 	ShoppingList, err := model.NewShoppingList(name, userId)
--- main.go
+++ main.go
@@ -3,11 +3,35 @@ package main
 import (
 	"github.com/dsbarabash/shopping-lists/internal/repository"
 	"github.com/dsbarabash/shopping-lists/internal/service"
+	"sync"
+	"time"
 )
 
 func main() {
-	NewList := service.CreateShoppingList("test_list", "test_user")
-	repository.CheckInterface(NewList)
-	NewItem := service.CreateItem("test_item", "test_comment", "test_user", "123")
-	repository.CheckInterface(NewItem)
+	ch := make(chan interface{})
+	stop := time.NewTimer(10 * time.Second)
+	defer close(ch)
+	logging := time.NewTicker(time.Millisecond * 200)
+	tick := time.NewTicker(time.Millisecond * 150)
+	defer tick.Stop() // освободим ресурсы, при завершении работы функции
+	defer logging.Stop()
+	for {
+		select {
+		case <-stop.C:
+			// stop - Timer, который через 10 секунд даст сигнал завершить работу
+
+			return
+		case <-tick.C:
+			// tick - Ticker, посылающий сигнал выполнить работу каждые 150 миллисекунд
+			wg := new(sync.WaitGroup)
+			wg.Add(2)
+			go service.CreateRandomStructs(ch)
+
+			go repository.CheckInterface2(ch)
+			wg.Done()
+		case <-logging.C:
+			// tick - Ticker, посылающий сигнал выполнить лоигрвоание каждые 200 миллисекунд
+			go repository.LoggingSlice()
+		}
+	}
 }
